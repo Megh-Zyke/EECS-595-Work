5121edea309810c7616365bce5c07d99e5edf8f6
Megh-Zyke <ms8615@srmist.edu.in>
2025-11-04 17:59:45 -0500

    something

--- tables.py
+++ tables.py
@@ -8,7 +8,7 @@ values = 0
 final_table = None
 
 for filename in os.listdir(file):
-    if filename.endswith('.csv') :
+    if filename.endswith('_filtered.csv') :
         if filename.startswith('jobs_export'):
             continue
 
@@ -23,5 +23,5 @@ for filename in os.listdir(file):
 print(f'Total number of job listings in all CSV files: {values}')
 
 final_table = final_table.drop_duplicates().reset_index(drop=True)
-out_path = os.path.join(os.path.dirname(__file__), "jobspy.csv")
+out_path = os.path.join(os.path.dirname(__file__), "filtered_jobs.csv")
 final_table.to_csv(out_path, index=False, encoding='utf-8')
--- testing_df.py
+++ testing_df.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
-non_tech_jobs = pd.read_csv('all_non_tech_jobs.csv')
+df = pd.read_csv("filtered_jobs.csv")
 
-non_tech_jobs.head()
+print("DataFrame Info:")
+print(df.info())
+
